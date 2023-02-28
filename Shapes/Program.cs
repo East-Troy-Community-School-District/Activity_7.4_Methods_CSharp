4afb20de520feb71a1a064ac1b3b375d7ee5f293
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2023-02-27 21:39:14 -0600

    Add files via upload

---
+++ Shapes/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Shapes
+ * 2/27/2023
+ * C#.NET I
+ * 
+ * Instructions
+ * Take a look at the Rectangle class.
+ * 1. How many instance variables does the class have?
+ * 2. How many properties does the class have?
+ * 3. How many constructors does the class have?
+ * 4. How many methods does the class have?
+ * 
+ * Let's add the code to create three Rectangle objects in an array.
+ * The first will have a length of 5 and 100, the second will have
+ * a length and width of 10, and the final will have a length and
+ * width of 20. Next, let's write the code to calculate the total
+ * area and perimeter of all three rectangles and display the results
+ * to the console. In addition, let's write the code to count the number
+ * of rectangles that are actually squares.
+ * 
+ * Let's create a new class called Circle, which will represent 
+ * a circle with a given radius. Let's use a property to store
+ * the radius. We will write only one constructor, which accept
+ * a radius and store it in the property. Additionally, we will write
+ * three methods: Area(), Circumference(), and Diameter(). Obviously, we
+ * should test our class by making some objects. Let's create a Circle
+ * object with a radius of 5. We will then display the area, circumference,
+ * and diameter of the circle to the console.
+ * 
+ * Finally, let's print each Rectangle object to the console and observe
+ * the result. What method was called to display the objects?
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add your code here!
+            
+        }
+    }
+}
---
+++ Shapes/Rectangle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    /// <summary>
+    /// Represents a rectangle.
+    /// </summary>
+    public class Rectangle
+    {
+        /// <summary>
+        /// Get or set the length of the rectangle.
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// Get or set the width of the rectangle.
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Constructs a rectangle with a length and width of 10.
+        /// </summary>
+        public Rectangle() : this(10, 10) { }
+
+        /// <summary>
+        /// Constructors a rectangle with a given length and width.
+        /// </summary>
+        /// <param name="length">Length of the rectangle.</param>
+        /// <param name="width">Width of the rectangle.</param>
+        public Rectangle(int length, int width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        /// <summary>
+        /// Calculates the area of the rectangle.
+        /// </summary>
+        /// <returns>The area of the rectangle.</returns>
+        public int Area()
+        {
+            return Length * Width;
+        }
+
+        /// <summary>
+        /// Calculates the perimeter of the rectangle.
+        /// </summary>
+        /// <returns>The perimeter of the rectangle.</returns>
+        public int Perimeter()
+        {
+            return 2 * Length + 2 * Width;
+        }
+
+        /// <summary>
+        /// Reports whether the rectangle is a square.
+        /// </summary>
+        /// <returns>
+        /// Returns true if the rectangle is a square; false otherwise.
+        /// </returns>
+        public bool IsSquare()
+        {
+            return Length == Width;
+        }
+
+        /// <summary>
+        /// Converts the object into a string.
+        /// </summary>
+        /// <returns>
+        /// A string in the format "Rectangle with length = {Length}
+        /// and width = {Width}".
+        /// </returns>
+        public override string ToString()
+        {
+            return "Rectangle with length = " + Length 
+                + " and width = " + Width;
+        }
+    }
+}
